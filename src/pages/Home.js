0ab1d4ba61dbca2285997d1fade5d62c5115722e
ShotAlex <shot.aleks@gmail.com>
2021-01-26 16:18:43 +0300

    feat: add basic template of th Search

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Search from "../components/Search/Search";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <>
+      <Search />
+
+    </>
   );
 };
 
