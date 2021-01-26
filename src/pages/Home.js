80a85728c2257611d3cf69c1ed32ac379ba940cc
ShotAlex <shot.aleks@gmail.com>
2021-01-26 16:28:36 +0300

    feat: add Cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,26 @@
 import React from 'react'
-import Search from "../components/Search/Search";
+import Search from '../components/Search/Search'
+import Cards from "../components/Cards/Cards";
 
 const Home = () => {
+  const cards = new Array(10)
+    .fill('')
+    .map((_, index) => {
+      return (
+        <div className='col-sm-4 mb-4' key={index}>
+          <Cards/>
+        </div>
+      )
+    })
+
+  console.log(cards)
   return (
     <>
       <Search />
 
+      <div className='row'>
+        {cards}
+      </div>
     </>
   );
 };
